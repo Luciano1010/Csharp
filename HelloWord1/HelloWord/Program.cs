31cb62554a7f7ff7cee3d39186693551af8ad240
Luciano1010 <luciano_lopesdealmeida@hotmail.com>
2023-08-22 11:09:14 -0300

    Aula 01 - Hello Word

---
+++ HelloWord1/HelloWord/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("A turma .NET 03 é top");
+        }
+
+    }
+}

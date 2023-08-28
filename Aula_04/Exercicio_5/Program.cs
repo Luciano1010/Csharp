fbdd23be2cd391d4491d2f2177413637885d313c
Luciano1010 <luciano_lopesdealmeida@hotmail.com>
2023-08-28 20:01:39 -0300

    Aula 05

---
+++ Aula_04/Exercicio_5/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+
+namespace Exercicio_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num,par = 0, impar = 0,contador;
+
+            for ( contador = 0; contador < 10; contador ++)
+            {
+
+                Console.WriteLine(" Digite um numero:");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                if (num % 2 == 0 && num > 0)
+                {
+                    par++;
+                   
+
+                    
+                }
+                else if (num % 2 != 0 && num > 0)
+                {
+                    impar++;
+                    
+
+                }
+               
+
+            }
+                Console.WriteLine($" numero par {par}");
+                Console.WriteLine($" numero impar{impar} ");
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
